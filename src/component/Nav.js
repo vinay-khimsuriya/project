0888bb8f1c3d7c41997a7cbc35b0a6bba51baec0
vinaykhim <vinay@glasier.in>
2024-11-26 18:02:10 +0530

    tailwind added

---
+++ src/component/Nav.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = () => {
+  return <div className="text-red-400">Navbar</div>;
+};
+
+export default Nav;
--- src/component/Navbar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Navbar = () => {
-  return <div>vinay</div>;
-};
-
-export default Navbar;
