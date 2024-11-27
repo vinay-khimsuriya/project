cbadc2415b5b616ae00e7e3aa3ff2bb378da8b5f
vinaykhim <vinay@glasier.in>
2024-11-27 12:59:52 +0530

    dorpdpwn is added

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -4,6 +4,7 @@ import { RxCross1 } from "react-icons/rx";
 
 const Nav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
   const navOption = [
     "Store",
     "Mac",
@@ -16,10 +17,12 @@ const Nav = () => {
     "Accessories",
     "Support",
   ];
+  const category = ["Shop", "Quick Links", "Shop Special Stores"];
+  const subCategory = ["Shop", "Quick Links", "Shop Special Stores", "abcd"];
   return (
-    <nav className="w-full bg-black flex justify-center">
+    <nav className="w-full bg-black flex flex-col items-center justify-center relative z-50">
       <div className="w-full lg:w-2/3">
-        <div className="w-full flex items-center justify-between gap-5 px-5">
+        <div className="w-full flex items-center bg-black justify-between gap-5 px-5">
           <div className=" cursor-pointer">
             <span className="hidden custom:block">
               <svg
@@ -51,8 +54,40 @@ const Nav = () => {
           <div className="text-white w-full hidden custom:flex justify-center ">
             <ul className="w-full flex flex-nowrap text-nowrap items-center gap-2 justify-between h-full">
               {navOption.map((option, index) => (
-                <li className="cursor-pointer" key={index}>
-                  <a>{option}</a>
+                <li
+                  className="cursor-pointer"
+                  key={index}
+                  onMouseEnter={() => {
+                    setIsHovered(true);
+                  }}
+                  onMouseLeave={() => {
+                    setIsHovered(false);
+                  }}
+                >
+                  <a className="py-3">{option}</a>
+
+                  <div
+                    className={`${isHovered ? "top-[95%]" : "-top-[450%]"}
+                      hidden -z-10 custom:flex justify-center top-full transition-all duration-1000 absolute w-screen left-0 bg-black text-white`}
+                  >
+                    <div className="w-2/3 text-start flex gap-5 p-5  self-start">
+                      {category.map((category, index) => (
+                        <ul className="">
+                          <li>
+                            <a>{category}</a>
+                          </li>
+                          {subCategory.map((category, index) => (
+                            <ul>
+                              <li>
+                                {" "}
+                                <a>{category}</a>
+                              </li>
+                            </ul>
+                          ))}
+                        </ul>
+                      ))}
+                    </div>
+                  </div>
                 </li>
               ))}
             </ul>
